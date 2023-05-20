28af8aed7bfa567dff0112885181812b68cc79a0
vinhpna1c <anhvinhphan659@gmail.com>
2023-05-20 23:00:18 +0700

    add submission

--- src/adapters/appService/submission.service.ts
+++ src/adapters/appService/submission.service.ts
@@ -1,6 +1,7 @@
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-import { formatResponse, getWithPath } from '../api.http';
+import { formatResponse, getWithPath, postWithPath } from '../api.http';
 
 import { ResponseData } from '~/constant';
 import API from '~/constant/api';
@@ -8,7 +9,10 @@ import { SubRole } from '~/constant/enum';
 import { Assignment } from '~/domain/assignment';
 import { Submission } from '~/domain/submission';
 import { SubmissionDTO, submissionFromDTO } from '~/dto/submission';
+import LocalStorage from '~/libs/LocalStorage/LocalStorage';
 import { mockAssignment } from '~/mock/assignment.mock';
+import moment from 'moment';
+import { RcFile } from 'antd/lib/upload';
 
 export function useSubmission() {
   const navigate = useNavigate();
@@ -45,6 +49,41 @@ export function useSubmission() {
       // const data = await getWithPath(`${API.PARTNER.GET.PARTNERS}/${id}`, {});
       const data = await mockAssignment().getAssignmentById(id);
       return formatResponse(data);
+    },
+    async addSubmission(
+      data: {
+        timemodified?: string;
+        origin?: string;
+        submitType: string;
+        link?: string;
+        file?: File | Blob | RcFile;
+      },
+      assignment: Assignment
+    ) {
+      const submitForm = new FormData();
+      submitForm.append('assignmentId', assignment.id);
+      submitForm.append('timemodified', moment().toISOString());
+      submitForm.append('origin', 'origin');
+      submitForm.append('submitType', data.submitType);
+
+      if (data.submitType.indexOf('sys') >= 0) {
+        submitForm.append('file', data.file ?? '');
+      } else {
+        submitForm.append('link', data.link ?? '');
+      }
+      const accessToken = (await LocalStorage.get(['accessToken'])).accessToken
+        .token;
+      const respond = await axios({
+        url: `http://localhost:5000/api/submission/${assignment.courseId}/${assignment.id}`,
+        data: submitForm,
+        method: 'post',
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      return respond;
     },
   };
 }
--- src/ui/modules/my-course/components/submission/AddSubmission.tsx
+++ src/ui/modules/my-course/components/submission/AddSubmission.tsx
@@ -1,66 +1,79 @@
 /* eslint-disable react/destructuring-assignment */
-import React, { useState } from "react";
-import { Button, Select, UploadProps } from "antd";
+import React, { useCallback, useState } from 'react';
+import { Button, Select, UploadProps } from 'antd';
 import './style.css';
-import { Option } from "antd/lib/mentions";
-import Input from "antd/lib/input/Input";
-import Dragger from "antd/lib/upload/Dragger";
-import { InboxOutlined } from "@ant-design/icons";
+import { Option } from 'antd/lib/mentions';
+import Input from 'antd/lib/input/Input';
+import Dragger from 'antd/lib/upload/Dragger';
+import { InboxOutlined } from '@ant-design/icons';
+import { Assignment } from '~/domain/assignment';
+import { RcFile } from 'antd/lib/upload';
+import { useSubmission } from '~/adapters/appService/submission.service';
 
 export enum SubmissionSource {
-  GIT = "Git",
-  GOOGLE_DRIVE = "Google Drive",
-  MOODLE = "Moodle",
-  SYSTEM = "System",
+  GIT = 'Git',
+  DRIVE = 'Drive',
+  MOODLE = 'Moodle',
+  SYSTEM = 'System',
 }
 
-const AddSubmission: React.FC = () => {
+type AddSubmissionProps = {
+  assignment: Assignment;
+};
+
+const AddSubmission: React.FC<AddSubmissionProps> = (props) => {
   const [isAddSubmission, setIsAddSubmission] = useState<boolean>(false);
+  const { assignment } = props;
   return (
     <div>
       <div className="gap-4">
-        <p className="submission-title">Assigment name</p>
-        <p>Assignment's Description</p>
+        <p className="submission-title">{assignment.name}</p>
+        <p>{assignment.description}</p>
       </div>
 
-      {isAddSubmission === false ?
+      {isAddSubmission === false ? (
         <>
           <SubmmissionStatusSection />
-          <div style={{ marginTop: 16, display: 'flex', justifyContent: 'center' }}>
-            <button className="ant-btn ant-btn-default" onClick={() => { setIsAddSubmission(true) }}>
+          <div
+            style={{ marginTop: 16, display: 'flex', justifyContent: 'center' }}
+          >
+            <button
+              className="ant-btn ant-btn-default"
+              onClick={() => {
+                setIsAddSubmission(true);
+              }}
+            >
               Add submission
             </button>
           </div>
-
         </>
-
+      ) : (
         // eslint-disable-next-line no-console
-        : <AddSubmissionSection onSubmitted={() => {
-          console.info("Ok back")
-          setIsAddSubmission(false)
-        }}
-        />}
-
-
+        <AddSubmissionSection
+          assignment={assignment}
+          onSubmitted={() => {
+            setIsAddSubmission(false);
+            location.reload();
+          }}
+        />
+      )}
     </div>
   );
-}
+};
 
-const SubmissionRow = (props: { title?: string, content?: string }): JSX.Element => {
+const SubmissionRow = (props: {
+  title?: string;
+  content?: string;
+}): JSX.Element => {
   return (
     <tr className="submission-row">
-      <td className="submission-field-td">
-        {props.title ?? ""}
-      </td>
-      <td className="submission-content-td">
-        {props.content ?? ""}
-      </td>
+      <td className="submission-field-td">{props.title ?? ''}</td>
+      <td className="submission-content-td">{props.content ?? ''}</td>
     </tr>
   );
-}
+};
 
 const SubmmissionStatusSection = (): JSX.Element => {
-
   return (
     <div style={{ marginTop: 8 }}>
       <p className="submission-title">Submission status</p>
@@ -76,17 +89,59 @@ const SubmmissionStatusSection = (): JSX.Element => {
         </table>
       </div>
     </div>
-  )
-}
+  );
+};
 
 // eslint-disable-next-line @typescript-eslint/ban-types
-const AddSubmissionSection = (props: { onSubmitted?: Function, }): JSX.Element => {
+const AddSubmissionSection = (props: {
+  onSubmitted?: Function;
+  assignment: Assignment;
+}): JSX.Element => {
+  const { assignment, onSubmitted } = props;
+  const { addSubmission } = useSubmission();
+  // handle submit data
+  let linkUrl = '';
+  const [submissionSrc, setSubmissionSrc] = useState<string>('System');
+  const [submittedFile, setSubmittedFile] = useState<
+    File | Blob | RcFile | undefined
+  >(undefined);
+
+  const submitSubmission = async () => {
+    // const submitForm = new FormData();
+    // submitForm.append('assignmentId', assignment.id);
+    // submitForm.append('timemodified', Date.now().toLocaleString());
+    // submitForm.append('origin', 'origin');
+    // submitForm.append('submitType', submissionSrc.toLowerCase());
+    // submitForm.append('link', linkUrl);
+    // if (submissionSrc.toLowerCase().indexOf('sys') >= 0) {
+    //   submitForm.append('file', linkUrl);
+    // } else {
+    //   submitForm.append('link', linkUrl);
+    // }
+    // submitForm.append('note', null);
+    // submitForm.append('userId', '');
+
+    // submitForm.append("grade","");
+    // submitForm.append("status","");
+    // submitForm.append("submissionMoodleId","");
+
+    const respond = await addSubmission(
+      {
+        submitType: submissionSrc.toLowerCase(),
+        link: linkUrl,
+        file: submittedFile,
+      },
+      assignment
+    );
+  };
+
   const uploadProps: UploadProps = {
     name: 'file',
-    multiple: true,
+    multiple: false,
     // action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
     onChange(info) {
       const { status } = info.file;
+
       if (status !== 'uploading') {
         console.log(info.file, info.fileList);
       }
@@ -97,69 +152,107 @@ const AddSubmissionSection = (props: { onSubmitted?: Function, }): JSX.Element =
       }
     },
     onDrop(e) {
-      console.log('Dropped files', e.dataTransfer.files);
+      console.log('Dropped files', e.dataTransfer.files.item(0));
     },
+    beforeUpload: (file) => {
+      console.info(`File before upload: ${JSON.stringify(file)}`);
+      setSubmittedFile(file);
+      return false;
+    },
+
   };
-  const [submissionSrc, setSubmissionSrc] = useState<string>('System');
-  const { onSubmitted } = props;
-  console.info("Submission src: " + submissionSrc)
+
+
+
   return (
     <div className="add-submission-container">
-
       <div>
-        <div className="submission-field" style={{ float: 'left' }}>Source origin: </div>
+        <div className="submission-field" style={{ float: 'left' }}>
+          Source origin:{' '}
+        </div>
         <Select
           defaultValue="System"
           onSelect={(value) => {
-            setSubmissionSrc(value)
+            setSubmissionSrc(value);
+            if (value.toLowerCase().indexOf('sys') >= 0) {
+              linkUrl = '';
+            } else {
+              setSubmittedFile(undefined);
+            }
           }}
           style={{ width: 200 }}
         >
-          {
-            Object.values(SubmissionSource).map((value) =>
-              <Option key={value} value={value}>{value}</Option>
-            )
-          }
+          {Object.values(SubmissionSource).map((value) => (
+            <Option key={value} value={value}>
+              {value}
+            </Option>
+          ))}
         </Select>
-
       </div>
       <div style={{ marginTop: 8 }}>
-        <div className="submission-field" style={{ float: 'left' }}>Path: </div>
+        <div className="submission-field" style={{ float: 'left' }}>
+          Path:{' '}
+        </div>
 
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          {
-            submissionSrc === 'System' ?
-              <Dragger {...uploadProps} style={{ width: '-moz-available' }} >
-                <p className="ant-upload-drag-icon">
-                  <InboxOutlined />
-                </p>
-                <p className="ant-upload-text">Click or drag file to this area to upload</p>
-                <p className="ant-upload-hint">
-                  Support for a single or bulk upload. Maximum upload file's size is 20MB.
-                </p>
-              </Dragger>
-              : <Input placeholder="URL of submission zip file" />
-          }
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          {submissionSrc === 'System' ? (
+            <Dragger {...uploadProps} style={{ width: '-moz-available' }}>
+              <p className="ant-upload-drag-icon">
+                <InboxOutlined />
+              </p>
+              <p className="ant-upload-text">
+                Click or drag file to this area to upload
+              </p>
+              <p className="ant-upload-hint">
+                Support for a single or bulk upload. Maximum upload file's size
+                is 20MB.
+              </p>
+            </Dragger>
+          ) : (
+            <Input
+              placeholder="URL of submission zip file"
+              onChange={(value) => {
+                linkUrl = value.target.value;
+              }}
+            />
+          )}
           <div>
-            <Button type='primary' style={{ backgroundColor: "#007c41", margin: 8, }} onClick={
-              () => {
-                //call api to add file
+            <Button
+              type="primary"
+              style={{ backgroundColor: '#007c41', margin: 8 }}
+              onClick={async () => {
+                if (onSubmitted !== undefined) {
+                  // call api to add file
+                  await submitSubmission();
+                  // back to previous screen
+                  onSubmitted();
+                }
+              }}
+            >
+              Save changes
+            </Button>
+            <Button
+              type="default"
+              title="Cancel"
+              onClick={() => {
                 if (onSubmitted != undefined) {
                   onSubmitted();
                 }
-              }} >Save changes</Button>
-            <Button type='default' title='Cancel' onClick={
-              () => {
-                if (onSubmitted != undefined) {
-                  onSubmitted();
-                }
-              }}>Cancel</Button>
+              }}
+            >
+              Cancel
+            </Button>
           </div>
         </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
 export default AddSubmission;
