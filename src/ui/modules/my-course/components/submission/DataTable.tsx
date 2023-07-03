45add9d1c75ee39ea5b59ec6b26e955279a608b3
Hidwin659 <anhvinhphan659@gmail.com>
2023-07-03 15:29:19 +0700

    update not submit display

--- src/ui/modules/my-course/components/submission/DataTable.tsx
+++ src/ui/modules/my-course/components/submission/DataTable.tsx
@@ -47,7 +47,7 @@ const DataTable: React.FC<{ courseId: string; assignmentId: string }> = ({
         statusStr = 'Error';
         break;
       default:
-        statusStr = 'Submitted';
+        statusStr = 'Not submitted';
         backgroundColor = 'gray';
     }
 
